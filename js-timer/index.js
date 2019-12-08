c091c05115d75db1cd3163a71cfa1b4ef7aed86f
Gerard Hynes <gkhynes@gmail.com>
2019-12-08 13:45:32 +0000

    Add onTick and onComplete callbacks

--- js-timer/index.js
+++ js-timer/index.js
@@ -5,6 +5,8 @@ class Timer {
     this.pauseButton = pauseButton;
     if (callbacks) {
       this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
     }
 
     this.startButton.addEventListener("click", this.start);
@@ -26,8 +28,14 @@ class Timer {
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
     } else {
       this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
     }
   };
 
@@ -48,6 +56,10 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log("Timer started");
   },
-  onTick() {},
-  onComplete() {}
+  onTick() {
+    console.log("Timer just ticked down");
+  },
+  onComplete() {
+    console.log("Timer is completed");
+  }
 });
