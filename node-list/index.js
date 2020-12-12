66b98bf722fad08203033534d389c8b7b0656064
Gerard Hynes <gkhynes@gmail.com>
2020-12-12 21:43:58 +0000

    Add callback-based solution

--- node-list/index.js
+++ node-list/index.js
@@ -1,8 +1,44 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
 
 fs.readdir(process.cwd(), (err, filenames) => {
   if (err) {
     console.log(err);
   }
-  console.log(filenames);
+
+  // Bad Version
+  // for (let filename of filenames) {
+  //   fs.lstat(filename, (err, stats) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //     console.log(filename, stats.isFile());
+  //   });
+  // }
+
+  // Better version 1
+  const allStats = Array(filenames.length).fill(null);
+
+  for (let filename of filenames) {
+    const index = filenames.indexOf(filename);
+
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+
+      allStats[index] = stats;
+
+      const ready = allStats.every((stats) => {
+        return stats;
+      });
+
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(filenames[index], stats.isFile());
+        });
+      }
+    });
+  }
 });
