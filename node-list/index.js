056629bdc573455e2cb52fa9b629fda801001d1a
Gerard Hynes <gkhynes@gmail.com>
2020-12-13 13:45:20 +0000

    Colour output with chalk

--- node-list/index.js
+++ node-list/index.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const util = require("util");
+const chalk = require("chalk");
 
 // Method #2
 // const lstat = util.promisify(fs.lstat);
@@ -68,7 +69,11 @@ fs.readdir(process.cwd(), async (err, filenames) => {
 
   for (let stats of allStats) {
     const index = allStats.indexOf(stats);
-    console.log(filenames[index], stats.isFile());
+    if (stats.isFile()) {
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.bold(filenames[index]));
+    }
   }
 });
 
