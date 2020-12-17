b95e92b82d87f3eeb223f70d7109a28aaeb00f80
Gerard Hynes <gkhynes@gmail.com>
2020-12-17 23:06:14 +0000

    Add initial routes

---
+++ ecomm/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" type="email" placeholder="email"/>
+        <input name="password" type="password" placeholder="password"/>
+        <input name-"passwordConfirmation" type="password" placeholder="password confirmation"/>
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post("/", (req, res) => {
+  res.send("Account created!");
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
