19b0f39eea59b831635a0a6f3d5998b32b8edf19
Gerard Hynes <gkhynes@gmail.com>
2020-12-30 22:41:15 +0000

    Add products routes

--- ecomm/index.js
+++ ecomm/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
+const productsRouter = require("./routes/admin/products");
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(
 
 // Connect routes
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000");
