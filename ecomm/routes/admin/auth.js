a3006ff7f46f8097a1f89a7327eb3f9c48f96be8
Gerard Hynes <gkhynes@gmail.com>
2021-01-03 12:31:54 +0000

    Add redirects to products page

--- ecomm/routes/admin/auth.js
+++ ecomm/routes/admin/auth.js
@@ -31,7 +31,7 @@ router.post(
     // Store id of user inside the user's cookie
     req.session.userId = user.id;
 
-    res.send("Account created!");
+    res.redirect("/admin/products");
   }
 );
 
@@ -54,7 +54,7 @@ router.post(
     const user = await usersRepo.getOneBy({ email });
 
     req.session.userId = user.id;
-    res.send("You are signed in!");
+    res.redirect("/admin/products");
   }
 );
 
