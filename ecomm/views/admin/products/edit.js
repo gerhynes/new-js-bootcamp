3a3702ad8883b7b390721ac295a39303927ab89d
Gerard Hynes <gkhynes@gmail.com>
2021-01-05 13:43:25 +0000

    Fix handleErrors middleware

--- ecomm/views/admin/products/edit.js
+++ ecomm/views/admin/products/edit.js
@@ -1,11 +1,14 @@
 const layout = require("../layout");
+const { getError } = require("../../helpers");
 
-module.exports = ({ product }) => {
+module.exports = ({ product, errors }) => {
   return layout({
     content: `
       <form method="POST">
         <input type="text" name="title" value="${product.title}" />
+        ${getError(errors, "title")}
         <input type="number" name="price" value="${product.price}" />
+        ${getError(errors, "price")}
         <input type="file" name="image" />
         <button>Submit</button>
       </form>
