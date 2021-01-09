7822fd5255c6ff471e999cd031f134206ea82e8a
Gerard Hynes <gkhynes@gmail.com>
2021-01-09 18:33:34 +0000

    Add shopping cart boilerplate

--- ecomm/index.js
+++ ecomm/index.js
@@ -4,9 +4,11 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/products");
 const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 
 const app = express();
 
+// Middleware
 // Middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,6 +22,7 @@ app.use(
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000");
---
+++ ecomm/routes/carts.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+// Receive POST request to add item to cart
+// Receive GET request to show all items in cart
+// Receive POST request to delete item from cart
+
+module.exports = router;
