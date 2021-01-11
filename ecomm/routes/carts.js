1851df63b5297cd3561ff3f7ed7e08f28da8b26e
Gerard Hynes <gkhynes@gmail.com>
2021-01-11 22:00:17 +0000

    Create cart

--- ecomm/routes/carts.js
+++ ecomm/routes/carts.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const cartsRepo = require("../repositories/carts");
 const router = express.Router();
 
 // Receive POST request to add item to cart
+router.post("/cart/products", async (req, res) => {
+  // Figure out cart
+  let cart;
+  if (!req.session.cartId) {
+    // We don't have a cart. Create one.
+    // Store cartId on req.session.cartId
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    // We have a cart. Get it from repository.
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+
+  console.log(cart);
+
+  // Either increment quantity
+  // OR
+  // Add new product
+
+  res.send("Product added to cart");
+});
 // Receive GET request to show all items in cart
 // Receive POST request to delete item from cart
 
