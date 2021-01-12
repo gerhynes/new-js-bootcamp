0edcc782ce53dbff7a7303069c41c7b0366d6d41
Gerard Hynes <gkhynes@gmail.com>
2021-01-12 21:08:20 +0000

    Add and style cart

--- ecomm/routes/carts.js
+++ ecomm/routes/carts.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
+
 const router = express.Router();
 
 // Receive POST request to add item to cart
@@ -34,7 +37,24 @@ router.post("/cart/products", async (req, res) => {
 
   res.send("Product added to cart");
 });
+
 // Receive GET request to show all items in cart
+router.get("/cart", async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect("/");
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
+});
+
 // Receive POST request to delete item from cart
 
 module.exports = router;
