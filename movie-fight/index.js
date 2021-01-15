a358babf3f1006ad49cd1ff671597d1fa660debc
Gerard Hynes <gkhynes@gmail.com>
2021-01-15 21:46:29 +0000

    Fetch movie data

--- movie-fight/index.js
+++ movie-fight/index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get(`https://www.omdbapi.com/`, {
+    params: {
+      apikey: "43f16007",
+      s: "avengers"
+    }
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
