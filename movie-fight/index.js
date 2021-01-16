22aee13efd8fe9b57f767fcf37499316e561bb02
Gerard Hynes <gkhynes@gmail.com>
2021-01-16 12:48:41 +0000

    Debounce fetchData

--- movie-fight/index.js
+++ movie-fight/index.js
@@ -1,12 +1,25 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get(`https://www.omdbapi.com/`, {
     params: {
       apikey: "43f16007",
-      s: "avengers"
+      s: searchTerm
     }
   });
 
   console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector("input");
+
+// Prevent excessive API requests
+let timeoutId;
+const onInput = (event) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 500);
+};
+
+input.addEventListener("input", onInput);
