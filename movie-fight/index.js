eb8cbc0ce23c59e3acb67acd875219c425635328
Gerard Hynes <gkhynes@gmail.com>
2021-01-17 13:01:48 +0000

    Refactor results dropdown

--- movie-fight/index.js
+++ movie-fight/index.js
@@ -13,20 +13,43 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 };
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label><b>Search for a Movie</b>
+  <input class="input" />
+  </label>
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>  
+  </div>
+`;
+
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
 
-  for (let movie of movies) {
-    const div = document.createElement("div");
+  // Clear any existing results
+  resultsWrapper.innerHTML = "";
 
-    div.innerHTML = `
-      <img src="${movie.Poster}" alt=${movie.Title}/>
-      <h2>${movie.Title}</h2>    
+  // New results dropdown
+  dropdown.classList.add("is-active");
+  for (let movie of movies) {
+    const option = document.createElement("a");
+
+    // Check for broken images
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+      <img src="${imgSrc}" alt=${movie.Title} />
+      <p>${movie.Title}</p>    
     `;
 
-    document.querySelector("#target").appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 };
 
