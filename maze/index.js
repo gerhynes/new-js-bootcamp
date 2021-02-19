bfce4d32f92d2091fddd68e2387a803244266971
Gerard Hynes <gkhynes@gmail.com>
2021-02-19 21:33:15 +0000

    Add random shapes

--- maze/index.js
+++ maze/index.js
@@ -1,4 +1,15 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const {
+  Engine,
+  Render,
+  Runner,
+  World,
+  Bodies,
+  Mouse,
+  MouseConstraint
+} = Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,12 +17,20 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: false,
+    width,
+    height
   }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+  })
+);
 
 // Walls
 const walls = [
@@ -22,4 +41,21 @@ const walls = [
 ];
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Random Shapes
+for (let i = 0; i < 50; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+        render: {
+          fillStyle: "green"
+        }
+      })
+    );
+  }
+}
