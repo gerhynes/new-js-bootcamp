1317bd3d3dd33d68964736c6da891ddc43c38c9b
Gerard Hynes <gkhynes@gmail.com>
2021-02-23 22:54:18 +0000

    Detect win condition

--- maze/index.js
+++ maze/index.js
@@ -1,6 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 20;
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -155,6 +155,7 @@ const goal = Bodies.rectangle(
   unitLength * 0.7,
   unitLength * 0.7,
   {
+    label: "goal",
     isStatic: true
   }
 );
@@ -162,7 +163,9 @@ const goal = Bodies.rectangle(
 World.add(world, goal);
 
 // Ball
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+  label: "ball"
+});
 World.add(world, ball);
 
 document.addEventListener("keydown", (event) => {
@@ -180,4 +183,18 @@ document.addEventListener("keydown", (event) => {
   if (event.keyCode === 65) {
     Body.setVelocity(ball, { x: x - 5, y });
   }
+});
+
+// Win Condition
+Events.on(engine, "collisionStart", (event) => {
+  event.pairs.forEach((collision) => {
+    const labels = ["ball", "goal"];
+
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      console.log("User won");
+    }
+  });
 });
